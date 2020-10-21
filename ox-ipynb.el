74722d4f4685c1db6e6e2e2409643f5375af9ca0
Leo Vivier <zaeph@zaeph.net>
2020-10-21 10:40:57 +0200

    Add header

---
+++ ox-ipynb.el
@@ -0,0 +1,37 @@
+;;; ox-ipynb.el --- Org-mode exporter for Jupyter notebooks  -*- fill-column: 78; lexical-binding: t; -*-
+
+;; Copyright © 2020 Leo Vivier <Symbol’s value as variable is void: zp/email-dev>
+
+;; Author: Leo Vivier <Symbol’s value as variable is void: zp/email-dev>
+;; URL: https://github.com/zaeph/ox-ipynb
+;; Keywords: org, jupyter
+;; Version: 0.1.0
+;; Package-Requires: org
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; This package provides an org-mode exporter for Jupyter notebooks.
+
+;;; Code:
+;;;; Library Requires
+(require 'org)
+
+(provide 'ox-ipynb)
+;;; ox-ipynb.el ends here
